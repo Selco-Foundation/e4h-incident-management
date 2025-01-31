10d77e692070d1774ab9ae504b8b9527081891e9
deepikaarora88 <118954890+deepikaarora88@users.noreply.github.com>
2025-01-31 15:19:45 +0000

    added docs

---
+++ backend/docs/StandaloneJSONProject/demo/src/main/java/com/readexcel/demo/entity/JsonResponse.java
@@ -0,0 +1,21 @@
+package com.readexcel.demo.entity;
+
+public class JsonResponse {
+	
+	private String json;
+	private String tennantId;
+	
+	public String getTennantId() {
+		return tennantId;
+	}
+	public void setTennantId(String tennantId) {
+		this.tennantId = tennantId;
+	}
+	public String getJson() {
+		return json;
+	}
+	public void setJson(String json) {
+		this.json = json;
+	}
+
+}
