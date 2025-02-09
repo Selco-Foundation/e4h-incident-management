06426a4b43f1871f5a09536c2b00c6c834f94b83
K Anirudh <anirudh.k@beehyv.com>
2025-02-10 05:01:54 +0530

    remove unutilized code

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/pgr/useInboxData.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/hooks/pgr/useInboxData.js
@@ -1,37 +1,38 @@
 import { useQuery, useQueryClient } from "react-query";
 import React, {useEffect, useState, useRef } from "react";
 import { useTranslation } from "react-i18next";
+
 const useInboxData = (searchParams,tenantIdNew) => {
   const { t } = useTranslation();
   let serviceIds = [];
   const inboxTotal=sessionStorage.getItem("inboxTotal");
-    let commonFilters = { start: 1, end: 10 };
-    const { limit, offset } = searchParams;
-    sessionStorage.setItem("limit", JSON.stringify(limit));
-    sessionStorage.setItem("offset", JSON.stringify(offset));
-    let appFilters = { ...commonFilters, ...searchParams?.filters?.pgrQuery, ...searchParams?.search, limit, offset };
-    sessionStorage.setItem("appFilters", JSON.stringify(appFilters));
-    sessionStorage.setItem("searchParams", JSON.stringify(searchParams));
-   let wfFilters
-    if(searchParams?.filters?.wfFilters?.assignee?.[0]?.code !=="")
-    {
-      wfFilters = { ...commonFilters, ...searchParams?.filters?.wfQuery,assignee:searchParams?.filters?.wfFilters?.assignee?.[0]?.code}
-    }
-    else {
-      wfFilters = { ...commonFilters, ...searchParams?.filters?.wfQuery}
-    }
-    const { assignee }=wfFilters;
-    const filterData=(data, appFilters)=>{
-      //const {phcType, incidentType, incidentId, applicationStatus, start, limit, offset, end}=appFilters;
-      const filteredItems= data.data.items;
-      const totalItems=data.data.totalCount;
-      const statusArray=data.data.statusMap;
-      // const sortedItems=filteredItems.sort((a,b)=>{
-      //   return b.businessObject?.auditDetails?.lastModifiedTime-a.businessObject?.auditDetails?.lastModifiedTime;
-      // })
-      //const paginationItems=filteredItems.slice(limit, offset+limit); 
-      return {total: totalItems, items:filteredItems, statusarray: statusArray};
-    };
+  let commonFilters = { start: 1, end: 10 };
+  const { limit, offset } = searchParams;
+  sessionStorage.setItem("limit", JSON.stringify(limit));
+  sessionStorage.setItem("offset", JSON.stringify(offset));
+  let appFilters = { ...commonFilters, ...searchParams?.filters?.pgrQuery, ...searchParams?.search, limit, offset };
+  sessionStorage.setItem("appFilters", JSON.stringify(appFilters));
+  sessionStorage.setItem("searchParams", JSON.stringify(searchParams));
+  let wfFilters
+  if(searchParams?.filters?.wfFilters?.assignee?.[0]?.code !=="")
+{
+    wfFilters = { ...commonFilters, ...searchParams?.filters?.wfQuery,assignee:searchParams?.filters?.wfFilters?.assignee?.[0]?.code}
+  }
+  else {
+    wfFilters = { ...commonFilters, ...searchParams?.filters?.wfQuery}
+  }
+  const { assignee }=wfFilters;
+  const filterData=(data, appFilters)=>{
+    //const {phcType, incidentType, incidentId, applicationStatus, start, limit, offset, end}=appFilters;
+    const filteredItems= data.data.items;
+    const totalItems=data.data.totalCount;
+    const statusArray=data.data.statusMap;
+    // const sortedItems=filteredItems.sort((a,b)=>{
+    //   return b.businessObject?.auditDetails?.lastModifiedTime-a.businessObject?.auditDetails?.lastModifiedTime;
+    // })
+    //const paginationItems=filteredItems.slice(limit, offset+limit); 
+    return {total: totalItems, items:filteredItems, statusarray: statusArray};
+  };
   const { data, isLoading, isFetching, isSuccess, refetch } = Digit.Hooks.useNewInboxGeneral({
     tenantId: Digit.ULBService.getCurrentTenantId(),
     ModuleCode: "Incident",
@@ -42,8 +43,8 @@ const useInboxData = (searchParams,tenantIdNew) => {
       },
       enabled: Digit.Utils.pgrAccess(),
     },
-      
-    
+
+
   });
   const filteredData= isSuccess && data ? filterData(data, appFilters) : {total:0, items:[], statusArray:[]};
   const client = useQueryClient();
@@ -59,8 +60,8 @@ const useInboxData = (searchParams,tenantIdNew) => {
     let tenantId = Digit.ULBService.getCurrentTenantId();
     const tenants = Digit.SessionStorage.get("Tenants")? Digit.SessionStorage.get("Tenants").map(item => item.code).join(','):"";
     const codes = Digit.SessionStorage.get("Tenants")?Digit.SessionStorage.get("Tenants").filter(item => item.code !== "pg")
-    .map(item => item.code)
-    .join(','):"";
+      .map(item => item.code)
+      .join(','):"";
     const sessionTenantId = Digit.SessionStorage.get("Employee.tenantId");
     const isCodePresent = (array, codeToCheck) =>{
       return array.some(item => item.code === codeToCheck);
@@ -75,38 +76,38 @@ const useInboxData = (searchParams,tenantIdNew) => {
     }
 
     //const tenant =  Digit.SessionStorage.get("Employee.tenantId") == "pg"?  Digit.SessionStorage.get("Tenants").map(item => item.code).join(',') :Digit.SessionStorage.get("Employee.tenantId") 
-    
-    
-    
+
+
+
     let complaintDetailsResponse = null;
     let incidentDetails=null;
     let combinedRes = [];
     if(filteredData!==undefined&& filteredData?.items?.length>0){
       incidentDetails= filteredData.items.map(incident=>incident.businessObject.incident);
     }
-  
+
     const workflowInstances=filteredData&& filteredData?.items.map(instances=>instances.ProcessInstance)
-    
+
     if (workflowInstances.length>0) {
       combinedRes = combineResponses(incidentDetails, workflowInstances).map((data) => ({
         ...data,
         sla: data.sla!=="-" ? Math.round(data.sla / (24 * 60 * 60 * 1000)) : "-",
       }));
-      
+
     }
-        
-   
+
+
     return {combinedRes:combinedRes, total:filteredData?.total, statusArray: filteredData?.statusarray};
-   
+
   };
 
   const result = fetchInboxData()
-  
-    return {data:result};
- };
+
+  return {data:result};
+};
 
 const mapWfBybusinessId = (wfs) => {
-    return wfs.reduce((object, item) => {
+  return wfs.reduce((object, item) => {
     let businessId="";
     if(item!==null){
       businessId=item["businessId"];
@@ -147,7 +148,7 @@ const combineResponses = (incidentDetails, workflowInstances) => {
         tenantId: incident.tenantId,
       })
     }});
-    
+
   return data;
 };
 
